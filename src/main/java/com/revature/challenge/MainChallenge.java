1eb856648f12515155c1c8b1fa847aced0289e86
ozzy castillo <oswaldo.castillo@revature.net>
2021-08-23 17:15:04 -0500

    Initial setup

---
+++ src/main/java/com/revature/challenge/MainChallenge.java
@@ -0,0 +1,14 @@
+package com.revature.challenge;
+
+public class MainChallenge {
+    
+    public static void main(String[] args) {
+    
+    }
+    
+    public boolean validateCard(String cardNumber){
+        
+        
+        return false;
+    }
+}
